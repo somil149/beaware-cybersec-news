93212075dedb5219def11bc8df0e52c2ede636ed
BeAware Team <dev@beaware.cybersectoday.org>
2026-05-16 07:08:44 +0530

    Add webpack alias configuration for @ path imports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
